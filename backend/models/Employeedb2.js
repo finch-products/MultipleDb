d717a9115ca35619618541ee30321e324ff153c8
Keerthi DL <42688856+keerthidl@users.noreply.github.com>
2020-07-08 17:55:09 +0530

    Update Employeedb2.js

--- backend/models/Employeedb2.js
+++ backend/models/Employeedb2.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/db2', {
-    useNewUrlParser: true
- }).then(() => {
-       console.log('Database sucessfully connected db2')
-    },
-    error => {
-       console.log('Database could not connected: ' + error)
-    }
- )
+// mongoose.connect('mongodb://localhost:27017/db2', {
+//     useNewUrlParser: true
+//  }).then(() => {
+//        console.log('Database sucessfully connected db2')
+//     },
+//     error => {
+//        console.log('Database could not connected: ' + error)
+//     }
+//  )
 
 // mongoose.connect('mongodb://localhost:27017/db2', {useNewUrlParser: true});
 
